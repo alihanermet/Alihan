ff361fe817fd807e9471239e15b9dd6f6e5255d6
Neutron <VoyagerHAN@github.com>
2019-09-28 16:00:53 +0300

    simple menu

---
+++ AtomProject/Assets/Script/Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Manager : MonoBehaviour
+{
+    public NetworkManager manager;
+    public GameObject NetManager;
+    void Awake()
+    {
+        if (manager == null)
+        {
+            manager = NetManager.GetComponent<NetworkManager>();
+        }
+    }
+
+    public void HostButton()
+    {
+        if (!manager.IsClientConnected() && !NetworkServer.active)
+        {
+            manager.StartHost();
+        }
+        
+    }
+
+    public void JoinButton()
+    {
+        if (!manager.IsClientConnected() && !NetworkServer.active)
+        {
+            manager.StartClient();
+        }
+
+    }
+
+}
