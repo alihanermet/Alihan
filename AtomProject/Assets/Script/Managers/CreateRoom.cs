31b814e5bd7951df16e32e1c27e6ca278a2e7f3d
Neutron <VoyagerHAN@github.com>
2019-09-29 20:30:23 +0300

    Ep02_Succsess

---
+++ AtomProject/Assets/Script/Managers/CreateRoom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CreateRoom : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text _roomName;
+    private Text RoomName { get { return _roomName; } }
+
+    public void ClickToCreateRoom()
+    {
+        if (PhotonNetwork.CreateRoom(RoomName.text))
+        {
+            print("create room succesfully sent.");
+        }
+        else
+        {
+            print("create room failed to sent.");
+        }
+    }
+
+    private void OnPhotonCreateRoomFailed(object[] codeAndMsg)
+    {
+        print("create room failed: " + codeAndMsg[1]);
+    }
+
+    private void OnCreatedRoom()
+    {
+        print("Room created succesfully");
+    }
+
+}
---
+++ AtomProject/Assets/Script/Managers/LobbyNetwork.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LobbyNetwork : MonoBehaviour
+{
+    private void Start()
+    {
+        print("Connecting to server...");
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    private void OnConnectedToMaster()
+    {
+        print("Connected to master");
+        PhotonNetwork.playerName = PlayerNetwork.Instance.PlayerName;
+
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    private void OnJoinedLobby()
+    {
+        print("Joined Lobby");
+    }
+
+}
